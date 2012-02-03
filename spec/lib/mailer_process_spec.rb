81ee89b8cb140666df58b9a41b1c990e8a69684d
Jason Taylor <jsntv200@gmail.com>
2012-02-03 16:06:12 +1100

    fix require so tests run

--- spec/lib/mailer_process_spec.rb
+++ spec/lib/mailer_process_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+require File.expand_path('../../spec_helper', __FILE__)
 require 'site_controller'
 SiteController.class_eval { def rescue_action(e) raise e; end }
 
